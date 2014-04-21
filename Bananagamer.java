c506cfdaaf266ddd6f7aa7cbc5795ccc8c519811
westonmizumoto <westonm1@stanford.edu>
2014-04-21 03:12:59 -0700

    got Compulsive Deliberation working

--- Bananagamer.java
+++ Bananagamer.java
@@ -1,5 +1,6 @@
 package org.ggp.base.player.gamer.statemachine.sample;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ggp.base.apps.player.detail.DetailPanel;
@@ -8,6 +9,7 @@ import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -24,33 +26,33 @@ import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
  * If you want to quickly create a gamer of your own, extend this class and
  * add the last core function : public Move stateMachineSelectMove(long timeout)
  */
-//test to see if this works
+
 public abstract class BananaGamer extends StateMachineGamer
 {
 	@Override
 	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
 	{
-//		long currentTime = System.currentTimeMillis();
-//		long finishBy = timeout - 1000;
-//
-//		StateMachine stateMachine = getStateMachine();
-//		MachineState rootState = getCurrentState();
-//		stateMachine.getInitialState();
-//
-//		MachineState currentState;
-//		int nbStatesExplored = 0;
-//		while(true){
-//			currentState = rootState;
-//			boolean isTerminal = stateMachine.isTerminal(currentState);
-//			while(!isTerminal){
-//				currentState = stateMachine.getRandomNextState(currentState);
-//				isTerminal = stateMachine.isTerminal(currentState);
-//				nbStatesExplored++;
-//			}
-//			if(System.currentTimeMillis() > finishBy)
-//				break;
-//		}
-//		System.out.println("Metagaming done. Nb states explored " + nbStatesExplored);
+		//		long currentTime = System.currentTimeMillis();
+		//		long finishBy = timeout - 1000;
+		//
+		//		StateMachine stateMachine = getStateMachine();
+		//		MachineState rootState = getCurrentState();
+		//		stateMachine.getInitialState();
+		//
+		//		MachineState currentState;
+		//		int nbStatesExplored = 0;
+		//		while(true){
+		//			currentState = rootState;
+		//			boolean isTerminal = stateMachine.isTerminal(currentState);
+		//			while(!isTerminal){
+		//				currentState = stateMachine.getRandomNextState(currentState);
+		//				isTerminal = stateMachine.isTerminal(currentState);
+		//				nbStatesExplored++;
+		//			}
+		//			if(System.currentTimeMillis() > finishBy)
+		//				break;
+		//		}
+		//		System.out.println("Metagaming done. Nb states explored " + nbStatesExplored);
 	}
 	@Override
 	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
@@ -64,23 +66,50 @@ public abstract class BananaGamer extends StateMachineGamer
 		//		long stop = System.currentTimeMillis();
 		//
 		//		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
-		//		return selection;
+		//		return selection;//end
+
 
 
 		long start = System.currentTimeMillis();//start of legal gamer
 		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
 		Move selection = moves.get(0);
+		//selection = getBestMove(getCurrentState(),move);
 		long stop = System.currentTimeMillis();
 		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
 		return selection;
 
 	}
+	public Move getBestMove(MachineState state, List<Move> moves) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+		int indexSave = 0;
+		int score = 0;
+		for(int i = 0; i < moves.size(); i++){
+			int result = maxScore(state);
+			if(result == 100){
+				return moves.get(i);
+			}
+			if(result > score){
+				indexSave = i;
+				score = result;
+			}
+		}
+		return moves.get(indexSave);
+	}
+	int maxScore(MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+		if (getStateMachine().isTerminal(state)) {
+			return getStateMachine().getGoal(state, getRole());
+		}
+		List<Move> moves = getStateMachine().getLegalMoves(state, getRole());
+		int score = 0;
+		for (int i = 0; i < moves.size(); i++){
+			List<Move> playerMoves = new ArrayList<Move>(); //type check ArrayList vs List
+			playerMoves.add(moves.get(i));
+			int result = maxScore(getStateMachine().getNextState(state, playerMoves));
+			if (result > score) score = result;
+		}
+		return score;
+	}
 
 
-	/** This will currently return "SampleGamer"
-	 * If you are working on : public abstract class MyGamer extends SampleGamer
-	 * Then this function would return "MyGamer"
-	 */
 	@Override
 	public String getName() {
 		return "BananaGamer";
