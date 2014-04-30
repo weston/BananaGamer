22228a58deb7bc33badfe981c5336783971c5609
westonmizumoto <westonm1@stanford.edu>
2014-04-30 14:54:04 -0700

    added some heuristics

--- Bananagamer.java
+++ Bananagamer.java
@@ -1,10 +1,7 @@
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.ggp.base.apps.player.detail.DetailPanel;
 import org.ggp.base.apps.player.detail.SimpleDetailPanel;
-import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
@@ -12,7 +9,6 @@ import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;//imported this
 import org.ggp.base.util.statemachine.StateMachine;
-import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
@@ -22,38 +18,17 @@ import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 
 
-public class BananaGamer extends StateMachineGamer
-{
+public class Minimax extends StateMachineGamer{
 
 	private static Role opponent;
 	private static final int perfectScore = 100;
-	private int alpha = 0;
-	private int beta = 100;
+	private int feasibility = 0;
+	private int limit = 4;
+	private static final double mobilityContribution = 0.95;
+	private static final double focusContribution = 0.05;
 	@Override
 	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
 	{
-		System.out.println(getCurrentState());
-		//		long currentTime = System.currentTimeMillis();
-		//		long finishBy = timeout - 1000;
-		//
-		//		StateMachine stateMachine = getStateMachine();
-		//		MachineState rootState = getCurrentState();
-		//		stateMachine.getInitialState();
-		//
-		//		MachineState currentState;
-		//		int nbStatesExplored = 0;
-		//		while(true){
-		//			currentState = rootState;
-		//			boolean isTerminal = stateMachine.isTerminal(currentState);
-		//			while(!isTerminal){
-		//				currentState = stateMachine.getRandomNextState(currentState);
-		//				isTerminal = stateMachine.isTerminal(currentState);
-		//				nbStatesExplored++;
-		//			}
-		//			if(System.currentTimeMillis() > finishBy)
-		//				break;
-		//		}
-		//		System.out.println("Metagaming done. Nb states explored " + nbStatesExplored);
 		for(Role role: getStateMachine().getRoles()){//finds opponent role
 			if(role != getRole())
 				opponent = role;
@@ -62,89 +37,121 @@ public class BananaGamer extends StateMachineGamer
 	@Override
 	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
 	{
-		long start = System.currentTimeMillis();//start of legal gamer
+		//System.out.println("test");
+		//		long start = System.currentTimeMillis();//taken from RandomGamer.java random gamer
+		//		//for nonrandom legal move take code from SampleLegalGamer.java
+		//
+		//		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		//		Move selection = (moves.get(new Random().nextInt(moves.size())));
+		//
+		//		long stop = System.currentTimeMillis();
+		//
+		//		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		//		return selection;//end
+
+
+
+		//long start = System.currentTimeMillis();//start of legal gamer
 		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
-		Move selection = getBestMove(getCurrentState(),moves);
-		long stop = System.currentTimeMillis();
-		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		Move selection = moves.get(0);
+		selection = getBestMove(getCurrentState());
+		//long stop = System.currentTimeMillis();
+		//notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
 		return selection;
 
 	}
-	public Move getBestMove(MachineState state, List<Move> moves) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
-		int indexSave = 0;
+	public Move getBestMove(MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(),getRole());
+		Move moveSave = moves.get(0);
 		int score = 0;
-		alpha = 0;
-		beta = 100;
-		System.out.println("getBestMove");
-		for(int i = 0; i < moves.size(); i++){
-			int result = minScore(getRole(),moves.get(i),state);
-			System.out.println(result + "this is the result "+ moves.get(i) +" this is the move");
+		for(Move move: moves){
+			int result = minScore(getRole(),move,state, 1);
 			if(result == perfectScore){
-				return moves.get(i);
+				return move;
 			}
 			if(result > score){
-				indexSave = i;
+				moveSave = move;
 				score = result;
 			}
-			System.out.println(score + "This is the score");
 		}
-		return moves.get(indexSave);
+		return moveSave;
 	}
 
-	int maxScore(Role myRole,MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+	int evalfn(MachineState state, Role myRole) throws MoveDefinitionException{
+		return (int) (mobilityContribution * mobility(state, myRole) + focusContribution * focus(state, myRole));
+	}
+
+	int mobility(MachineState state, Role myRole) throws MoveDefinitionException{ //IT'S A THREEEEEEE LINERRRRR!!!!!
+		int legalmoves = getStateMachine().getLegalMoves(state, myRole).size();
+		if (legalmoves + 1 > feasibility) feasibility = legalmoves + 1;
+		return legalmoves*100/feasibility;
+	}
+
+	int focus(MachineState state, Role myRole) throws MoveDefinitionException{ //IT'S A THREEEEEEE LINERRRRR!!!!!
+		int legalmoves = getStateMachine().getLegalMoves(state, myRole).size();
+		if (legalmoves + 1 > feasibility) feasibility = legalmoves + 1;
+		return 100 - legalmoves*100/feasibility;
+	}
+
+	int maxScore(Role myRole,MachineState state, int level) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
 		if (getStateMachine().isTerminal(state)) {
 			return getStateMachine().getGoal(state, getRole());
 		}
-		List<Move> moves = getStateMachine().getLegalMoves(state, getRole());
-		for (int i = 0; i < moves.size(); i++){
-			int result = minScore(getRole(),moves.get(i),state);//minmax
-			alpha = Math.max(alpha, result);
-			if (alpha>=beta) {
-				return beta;
+		if (level >= limit) return evalfn(state, myRole);//mobility(state, myRole);
+		List<Move> moves = getStateMachine().getLegalMoves(state, myRole);
+		int score = 0;
+		for (Move move: moves){
+			int result = minScore(getRole(),move,state, level + 1);
+						if(result == perfectScore){
+							return perfectScore;
+						}
+			if (result > score) {
+				score = result;
 			}
 		}
-		return alpha;
+		return score;
 	}
 
-	int minScore(Role myRole,Move myMove, MachineState state) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
-		List<Move> opponentMoves = getStateMachine().getLegalMoves(state,opponent);
-		//int currentLowestScore = perfectScore; //minmax
-		for(int i = 0; i < opponentMoves.size(); i++){
-			Move opponentMove = opponentMoves.get(i);
-			List<Move> jointMove = new ArrayList<Move>();
-			if( myRole == getStateMachine().getRoles().get(0)){
-				jointMove.add(myMove);
-				jointMove.add(opponentMove);
-			}else{
-				jointMove.add(opponentMove);
-				jointMove.add(myMove);
+	int minScore(Role myRole,Move myMove, MachineState state, int level) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException{
+
+		List<List<Move>> opponentMoves = getStateMachine().getLegalJointMoves(state, myRole, myMove);
+		if (opponentMoves.isEmpty())System.out.println("hello world");
+
+		int currentLowestScore = perfectScore;
+		for(List<Move> opponentMove: opponentMoves){
+			//if (opponentMove != myMove){
+			//List<Move> jointMove = new ArrayList<Move>();
+			//if( myRole == getStateMachine().getRoles().get(0)){
+			//	jointMove.add(myMove);
+			//	jointMove.add(opponentMove);
+			//}else{
+			//	jointMove.add(opponentMove);
+			//	jointMove.add(myMove);
+			//}
+			MachineState nextState = getStateMachine().getNextState(state,opponentMove);
+			int nextStateScore = maxScore(getRole(),nextState, level + 1);
+			//	if(nextStateScore == 0){
+			//		return 0;
+			//	}
+			if(nextStateScore < currentLowestScore){
+				currentLowestScore = nextStateScore;
 			}
-			//System.out.println("min");
-			MachineState nextState = getStateMachine().getNextState(state,jointMove);
-			int nextStateScore = maxScore(getRole(),nextState);
-			beta = Math.min(beta, nextStateScore);
-			if (beta <= alpha) return alpha;
-//			//if(nextStateScore == 0){//minmax
-//			//	return 0;
-//			//}
-			//if(nextStateScore < currentLowestScore){//minmax
-			//	currentLowestScore = nextStateScore;
 			//}
 		}
-		//return currentLowestScore;//minmax
-		return beta;
+		return currentLowestScore;
 	}
+
 
 	@Override
 	public String getName() {
-		return "BananaGamer";
+		return "BananaHeuristic";
 	}
 
 	// This is the default State Machine
 	@Override
 	public StateMachine getInitialStateMachine() {//hash map from moves to machine states?
-		return new CachedStateMachine(new ProverStateMachine());
-		//return new ProverStateMachine();
+		//return new CachedStateMachine(new ProverStateMachine());
+		return new ProverStateMachine();
 	}
 
 	// This is the default Sample Panel
